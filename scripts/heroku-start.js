55311d0d4986492b76b56c0001ebfca918fb3717
Alexander <mescherskiy.alexandr@gmail.com>
2023-09-18 18:06:22 +0200

    server.js created

--- scripts/heroku-start.js
+++ scripts/heroku-start.js
@@ -1,20 +1,20 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const httpProxy = require('http-proxy');
+// const httpProxy = require('http-proxy');
 const port = process.env.PORT || 3000;
 
-const proxy = httpProxy.createProxyServer();
+// const proxy = httpProxy.createProxyServer();
 
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '..', 'build')));
 app.get('/', (req,res) => {
   res.sendFile(path.join(__dirname, '..', 'build', 'index.html'));
 });
-app.all('/api/*', (req, res) => {
-  const backendURL = 'https://media-hosting-beedbd9a2f9f.herokuapp.com';
-  proxy.web(req, res, { target: backendURL });
-});
+// app.all('/api/*', (req, res) => {
+//   const backendURL = 'https://media-hosting-beedbd9a2f9f.herokuapp.com';
+//   proxy.web(req, res, { target: backendURL });
+// });
 app.get('*', (req,res) => {
   res.sendFile(path.join(__dirname, '..', 'build', 'index.html'));
 });
