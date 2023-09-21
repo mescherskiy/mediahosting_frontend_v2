79f23daa92c72129ff3fcb2d351bf0b0d989e314
Alexander <mescherskiy.alexandr@gmail.com>
2023-09-21 22:08:08 +0200

    edited heroku-start.js

--- scripts/heroku-start.js
+++ scripts/heroku-start.js
@@ -1,8 +1,6 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-// const httpProxy = require('http-proxy');
-// const { createProxyMiddleware } = require('http-proxy-middleware');
 const port = process.env.PORT || 3000;
 
 // const proxy = httpProxy.createProxyServer();
@@ -21,15 +19,15 @@ const port = process.env.PORT || 3000;
 // app.use("/", frontendProxy);
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '..', 'build')));
-app.get('/', (req,res) => {
-  res.sendFile(path.join(__dirname, '..', 'build', 'index.html'));
+app.get('/', function(req,res){
+  res.sendFile(path.join(__dirname, '..', 'build'));
 });
 // app.all('/api/*', (req, res) => {
 //   const backendURL = 'https://media-hosting-beedbd9a2f9f.herokuapp.com';
 //   proxy.web(req, res, { target: backendURL });
 // });
-app.get('*', (req,res) => {
-  res.sendFile(path.join(__dirname, '..', 'build', 'index.html'));
+app.get('*', function(req,res){
+  res.sendFile(path.join(__dirname, '..', 'build/index.html'));
 });
 app.listen(port, ()=>{
   console.log("Server is running on port: ", port)
